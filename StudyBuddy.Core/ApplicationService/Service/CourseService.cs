af2280d82ca6c64aa0da9c342d4588b9029f3bb3
Chililove <mega_lulu_kryster@hotmail.com>
2020-12-02 17:57:17 +0100

    deleting of entityfolder

--- StudyBuddy.Core/ApplicationService/Service/CourseService.cs
+++ StudyBuddy.Core/ApplicationService/Service/CourseService.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StudyBuddy.Core.ApplicationService.Service
 {
-    class CourseService
+    public class CourseService : ICourseService
     {
+        public Course Create(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course CreateCourse(long id, string name, List<Topic> topics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Course> GetCourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course Update(Course courseUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
