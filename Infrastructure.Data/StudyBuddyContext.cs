68976a5d6b1fde62bf0f316484a0cee4b3e1681a
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2020-12-03 22:18:00 +0100

    DbContext Created

---
+++ Infrastructure.Data/StudyBuddyContext.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class StudyBuddyContext : DbContext
+    {
+        public StudyBuddyContext(DbContextOptions<StudyBuddyContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Topic> Topic { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
