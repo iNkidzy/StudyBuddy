79f8002b6e4b01219c0d94bfd828d0be562737f8
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2020-12-04 15:36:25 +0100

    Authentication added

--- StudyBuddy.WebApi/Controllers/tokenController.cs
+++ StudyBuddy.WebApi/Controllers/tokenController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entity;
+using Infrastructure.Data.Helper;
+using Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,5 +13,34 @@ namespace StudyBuddy.WebApi.Controllers
     [Route("/token")]
     public class TokenController : Controller
     {
+        private readonly UserRepo _userRepo;
+        private readonly IAuthenticationHelper _authenticationHelper;
+
+        public TokenController(UserRepo userRepo, IAuthenticationHelper authHelper)
+        {
+            _userRepo = userRepo;
+            _authenticationHelper = authHelper;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginUserModel model)
+        {
+            var user = _userRepo.GetUser().FirstOrDefault(u => u.Name == model.Username);
+
+            // check if username exists
+            if (user == null)
+                return Unauthorized();
+
+            // check if password is correct
+            if (!_authenticationHelper.VerifyPasswordHash(model.Password, user.PasswordHash, user.PasswordSalt))
+                return Unauthorized();
+
+            // Authentication successful
+            return Ok(new
+            {
+                username = user.Name,
+                token = _authenticationHelper.GenerateToken(user)
+            });
+        }
     }
 }
---
+++ StudyBuddy.WebApi/Helper/JwtSecurityKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace StudyBuddy.WebApi.Helper
+{
+    public static class JwtSecurityKey
+    {
+        private static byte[] secretBytes = Encoding.UTF8.GetBytes("A secret for HmacSha256");
+
+        public static SymmetricSecurityKey Key
+        {
+            get { return new SymmetricSecurityKey(secretBytes); }
+        }
+
+        public static void SetSecret(string secret)
+        {
+            secretBytes = Encoding.UTF8.GetBytes(secret);
+        }
+    }
+}
